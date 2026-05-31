856ae859b88f0887eabf938950d0b6f9115f25ae
Babita Pakuwal <babitapakuwal505@gmail.com>
2026-05-31 15:51:06 +0200

    Style: improve watchlist action buttons (edit/delete/ghost variants)

--- watch_status_management/watchlist.php
+++ watch_status_management/watchlist.php
@@ -117,7 +117,7 @@ try {
                     </td>
                     <td class="center">
                         <div class="actions">
-                            <button class="act"
+                            <button class="act edit"
                                 onclick="openEditModal(<?php echo $statusId; ?>, <?php echo $movieId; ?>, '<?php echo addslashes($movie['title']); ?>', '<?php echo $state; ?>', <?php echo $progress; ?>)">
                                 <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.2"><path d="M12 20h9"/><path d="M16.5 3.5a2.1 2.1 0 0 1 3 3L7 19l-4 1 1-4z"/></svg>Edit
                             </button>
